25f87d5fe00dfd6bbb7d3283ad61746816601360
Fang-Pen Lin <hello@fangpenlin.com>
2022-04-03 15:41:18 -0700

    Add entry type

--- beancount_black/formater.py
+++ beancount_black/formater.py
@@ -1,3 +1,5 @@
+import collections
+import enum
 import io
 import logging
 import re
@@ -12,9 +14,77 @@ from lark.visitors import Visitor
 COMMENT_PREFIX = re.compile("[;*]+")
 
 
+@enum.unique
+class EntryType(enum.Enum):
+    # Date directives
+    OPEN = "OPEN"
+    CLOSE = "CLOSE"
+    BALANCE = "BALANCE"
+    EVENT = "EVENT"
+    COMMODITY = "COMMODITY"
+    DOCUMENT = "DOCUMENT"
+    NOTE = "NOTE"
+    PAD = "PAD"
+    TXN = "TXN"
+    # Simple directives
+    OPTION = "OPTION"
+    INCLUDE = "INCLUDE"
+    PLUGIN = "PLUGIN"
+    # Other
+    COMMENTS = "COMMENTS"
+
+
+DATE_DIRECTIVE_ENTRY_TYPES = {
+    "open": EntryType.OPEN,
+    "close": EntryType.CLOSE,
+    "balance": EntryType.BALANCE,
+    "event": EntryType.EVENT,
+    "commodity": EntryType.COMMODITY,
+    "document": EntryType.DOCUMENT,
+    "note": EntryType.NOTE,
+    "pad": EntryType.PAD,
+    "txn": EntryType.TXN,
+}
+
+SIMPLE_DIRECTIVE_ENTRY_TYPES = {
+    "option": EntryType.OPTION,
+    "include": EntryType.INCLUDE,
+    "plugin": EntryType.PLUGIN,
+}
+
+
+def get_entry_type(statement: Tree) -> EntryType:
+    first_child: Tree = statement.children[0]
+    if first_child.data == "date_directive":
+        return DATE_DIRECTIVE_ENTRY_TYPES[first_child.children[0].data]
+    elif first_child.data == "simple_directive":
+        return SIMPLE_DIRECTIVE_ENTRY_TYPES[first_child.children[0].data]
+    else:
+        raise ValueError(f"Unexpected first child type {first_child.data}")
+
+
 class StatementGroup(typing.NamedTuple):
     header: typing.Optional[Token]
     statements: typing.List[Tree]
+
+
+class Metadata(typing.NamedTuple):
+    comments: typing.List[Token]
+    statement: Tree
+
+
+class Posting(typing.NamedTuple):
+    comments: typing.List[Token]
+    statement: Tree
+    metadata: typing.List[Metadata]
+
+
+class Entry(typing.NamedTuple):
+    type: EntryType
+    comments: typing.List[Token]
+    statement: Tree
+    metadata: typing.List[Metadata]
+    postings: typing.List[Posting]
 
 
 class BeancountCollector(Visitor):
@@ -65,6 +135,67 @@ def format_comment(token: Token) -> str:
     if not remain:
         return prefix
     return f"{prefix} {remain}"
+
+
+def format_statement_group(group: StatementGroup) -> str:
+    lines: typing.List[str] = []
+    if group.header is not None:
+        lines.append(format_comment(group.header))
+        if group.statements:
+            lines.append("")
+
+    entries: typing.List[Entry] = []
+    comments: typing.List[Token] = []
+    for statement in group.statements:
+        first_child = statement.children[0]
+        if isinstance(first_child, Token):
+            if first_child.type == "COMMENT":
+                comments.append(statement)
+            else:
+                raise ValueError(f"Unepxected token {first_child.type}")
+        else:
+            if comments and comments[-1].line != statement.meta.line + 1:
+                # Standalone comment group
+                entry = Entry(
+                    type=EntryType.COMMENTS,
+                    comments=comments,
+                    statement=statement,
+                    metadata=[],
+                    postings=[],
+                )
+                entries.append(entry)
+                comments = []
+            if first_child.data == "posting":
+                last_entry = entries[-1]
+                if last_entry.type != EntryType.TXN:
+                    raise ValueError("Transaction expected")
+                last_entry.postings.append(
+                    Posting(comments=comments, statement=statement, metadata=[])
+                )
+                continue
+            elif first_child.data == "metadata_item":
+                last_entry = entries[-1]
+                metadata = Metadata(comments=comments, statement=statement)
+                if last_entry.postings:
+                    last_posting: Posting = last_entry.postings[-1]
+                    last_posting.metadata.append(metadata)
+                else:
+                    last_entry.metadata.append(metadata)
+                continue
+            entry = Entry(
+                type=get_entry_type(statement),
+                comments=comments,
+                statement=statement,
+                metadata=[],
+                postings=[],
+            )
+            entries.append(entry)
+
+    entry_groups = collections.defaultdict(list)
+    for entry in entries:
+        pass
+    print(entries)
+    return "\n".join(lines)
 
 
 def format_beancount(tree: ParseTree, output_file: io.TextIOBase):
